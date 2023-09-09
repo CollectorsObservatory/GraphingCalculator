b3acd5c9dbe5796bf658f3c0e561c5dc1d0dc3d4
Fares Majdoub <126903635+CollectorsObservatory@users.noreply.github.com>
2023-09-09 19:33:02 -0400

    Update Apollo_Calculator.py
    improved User interaction
    Added graphical only mode
    added more user guidance like the ability to use sin x ...

--- Apollo_Calculator.py
+++ Apollo_Calculator.py
@@ -3,7 +3,8 @@ import sympy as sp
 import numpy as np
 import matplotlib.pyplot as plt
 now = datetime.datetime.now()
-print(now.strftime("%Y-%m-%d %H:%M:%S"))
+current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+
 # basically a program with two modes basic and graphical calculator
 # graphical calculator calculates derivatives , integrals and plots a graphic
 
@@ -26,58 +27,58 @@ class Calculator:
         return self.num_1 * self.num_2
 
     def division(self):
-        if self.num_2 != 0:
+        try:
             return self.num_1 / self.num_2
-        else:
-            print("Eroor division by zero")
+        except ZeroDivisionError:
+            return "Can't Divide by Zero!"
 
     def power(self):
         return self.num_1 ** self.num_2
 
     def modulo(self):
         if self.num_1 % self.num_2 == 0:
-            print(f"{self.num_1} can be divided by {self.num_2}")
+            return f"{self.num_1} can be divided by {self.num_2}"
+
         else:
-            print("No full division possible!")
-        return " "
+            return "No full division possible!"
 
 
 # start of the program by giving the user choices to choose from
-print("   Welcome to Apollo Calculator   ")
-print("   Press 1- for Basic calculator")
-print("   Press 2- for graphing calculator")
-print("   Press ctrl+r to rest")
+print("   ***Welcome to Apollo Calculator***   ")    # Print a welcome message
+print(f"  Current time is {current_time}")
+print(" ")
+print("   Press 1- **Basic Calculator**")
+print("   Press 2- **Graphical Calculator**")
+print("   Press 3- **Plot Graphics directly**")
+print(" ")
 while True:
     try:
-        mode_choice = int(input("Enter your choice: "))
+        mode_choice = int(input("-Please choose the right mode: "))
         if mode_choice == 1:
             ans_list = []  # used later to store the last result to be used again
             # Define a list of valid operators
             operator_list = ["-", "+", "*", "**", "/", "%"]
 
-            # Print a welcome message
-            print("Welcome to Apollo 11 Calculator")
-
             # Define the output format
-            print(f"Possible operations are {operator_list}")
-            print("% for Modulo")
-            print("** for Power")
-            print("If you want to use your old result , type ans ")
+            print(f"-Possible operations are {operator_list}")
+            print("            -% for Modulo")
+            print("            -** for Power")
+            print("-If you want to use your old result , type ans in the terminal ")
 
             # Start an infinite loop to keep the calculator running
             while True:
                 try:
                     # Get the user's input for the first number
-                    number_1 = float(input("Please enter the first number: "))
+                    number_1 = float(input("-Please enter the first number: "))
                     # Get the user's input for the second number
                     number_2 = float(input("Please enter the second number: "))
                     # Get the user's choice of operation
-                    operator = str(input("Choose the operation: "))
+                    operator = str(input("-Choose the operation: "))
                     print(" ")
 
                     # Check if the operator is valid, if not, prompt the user to try again
                     if operator not in operator_list:
-                        print("Please try again")
+                        print("Please try again!")
 
                     # Create a Calculator instance with user inputs
                     calculation = Calculator(num_1=number_1, num_2=number_2, operation=operator)
@@ -115,12 +116,12 @@ while True:
                     print(calculator_suite(calculation, operator))
                     ans = ans_list[0]
 
-                    choice = input("Type ans to use old result, Press Enter to Continue, Type exit to quit: ")
+                    choice = input("-Type ans to use old result, Press Enter to Continue, Type exit to quit: ")
 
                     if choice.lower() == "ans":
                         while True:
-                            new_number_2 = float(input("Enter second number: "))
-                            new_operator = str(input("Enter operator: "))
+                            new_number_2 = float(input("-Enter second number: "))
+                            new_operator = str(input("-Enter operator: "))
                             calculation = Calculator(num_1=ans, num_2=new_number_2, operation=operator)
                             print(calculator_suite(calculation, new_operator))
                             ans_list.append(calculation.modulo())
@@ -131,17 +132,18 @@ while True:
                         continue  # Continue to the next iteration of the loop
 
                     elif choice.lower() == "exit":
-                        print("Closing the program")
+                        print("**Closing the program, see you soon !**")
                         break  # Exit the loop if the user wants to exit
 
                 except ValueError:
-                    print("Please try again")  # Handle invalid inputs
+                    print("*Wrong input, Please try again!*")  # Handle invalid inputs
         if mode_choice == 2:
             # now switching to graphical calculator , a class here is useless
-            print("NOTE: x is by default the symbol of the variable")
+            print("** x is by default the symbol of the variable **")
+            print("** WE HANDLE SIN FUNCTIONS **")
             while True:
 
-                function = input("Enter function: ")
+                function = input("-Enter function using x: ")
 
                 x = sp.symbols("x")   # x is the variable symbol by default as stated to the user
 
@@ -163,18 +165,19 @@ while True:
 
                 y_integrate_values = [f_integrate_func(x_val) for x_val in x_values]
 
-                action_input = str(input("type D for derivative or I for integration or B for both: "))
+                action_input = str(input("-Type D for derivative, I for integration or B for both: "))
                 if action_input.lower == "d":
                     print(derivative_function)
 
-                elif action_input.lower() == "t":
+                elif action_input.lower() == "i":
                     print(integrate_function)
 
                 elif action_input.lower() == "b":
-                    print(f"derivative is {derivative_function} integral is {integrate_function} + C")
+                    print(f"**Derivative is {derivative_function}  **")
+                    print(f"**Integral is {integrate_function} + C **")
 
-                view_input = str(input("Do you want to view the plot of the function? (Y for yes , N for no, "
-                                       "E to exit: )"))
+                view_input = str(input("-Do you want to view the plot of the function? (Y for yes , N for no, "
+                                       "E to exit program: "))
 
                 if view_input.lower() == "y":
                     # will open a window to display the function
@@ -189,11 +192,37 @@ while True:
                     plt.grid(True)
                     plt.show()
                 elif view_input.lower() == "n":
+                    print("T**he program resets**")
                     continue
                 else:
-                    print("Closing program")
+                    print("**Closing the program, see you soon !**")
                     break
+        if mode_choice == 3:
+            print("**Welcome to Graphical only mode**")
+            while True:
+                function = input("-Enter function using x: ")
+                x = sp.symbols("x")  # x is the variable symbol by default as stated to the user
+                f_func = sp.lambdify(x, function, 'numpy')
+                x_values = np.linspace(-15, 15, 400)
+                y_values = [f_func(x_val) for x_val in x_values]
+                plt.figure(figsize=(10, 8))
+                plt.plot(x_values, y_values, label=function)
+                plt.xlabel('x')
+                plt.ylabel('y')
+                plt.legend()
+                plt.title(f'Plot of the function:{function}')
+                plt.grid(True)
+                plt.show()
+                mode_3 = str(input("Do you wish to continue using this mode?(y or n): "))
+                try:
+                    if mode_3.lower() == "y":
+                        continue
+                    elif mode_3.lower() == "n":
+                        print("**Closing the program, see you soon !**")
+                        break
+                except ValueError:
+                    print("Wrong input, Reset the program and try again")
 
     except ValueError:
-        print("Please try again")  # Handle invalid inputs
+        print("**Wrong input, Please try again!**")  # Handle invalid inputs
 
